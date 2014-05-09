e0f508cde6da76a3c05e95d4924496c78cde86bf
Shawn Shojaie <shojaie.mac@gmail.com>
2014-05-08 18:13:34 -0700

    Working app? recieved from cameron

--- gen/com/davisECS/virtualfrontview/R.java
+++ gen/com/davisECS/virtualfrontview/R.java
@@ -25,15 +25,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int client_button=0x7f080002;
-        public static final int main_text=0x7f080000;
-        public static final int server_button=0x7f080001;
-        public static final int surface=0x7f080003;
+        public static final int action_settings=0x7f080008;
+        public static final int client_button=0x7f080006;
+        public static final int ip_text=0x7f080005;
+        public static final int linear_layout=0x7f080000;
+        public static final int main_text=0x7f080002;
+        public static final int server_button=0x7f080004;
+        public static final int surface=0x7f080007;
+        public static final int surface_view=0x7f080001;
+        public static final int user_ip=0x7f080003;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_server=0x7f030001;
+        public static final int activity_client=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_server=0x7f030002;
+        public static final int fragment_client=0x7f030003;
     }
     public static final class menu {
         public static final int client=0x7f070000;
--- src/com/davisECS/virtualfrontview/ServerActivity.java
+++ src/com/davisECS/virtualfrontview/ServerActivity.java
@@ -28,8 +28,8 @@ import android.widget.Button;
 import android.os.Build;
 import android.preference.PreferenceManager;
 
-public class ServerActivity extends Activity implements OnClickListener,
-		Session.Callback, SurfaceHolder.Callback, OnPreparedListener {
+public class ServerActivity extends Activity implements Session.Callback,
+		SurfaceHolder.Callback {
 
 	private final static String TAG = "VirtualFrontView";
 
@@ -38,7 +38,6 @@ public class ServerActivity extends Activity implements OnClickListener,
 	// For client video playback
 	MediaPlayer mediaPlayer;
 	SurfaceHolder surfaceHolder;
-	String videoSrc = "rtsp://10.0.1.51:1234";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,90 +45,86 @@ public class ServerActivity extends Activity implements OnClickListener,
 		setContentView(R.layout.activity_server);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
+		
 		mSurfaceView = (SurfaceView) findViewById(R.id.surface);
 
-				// Sets the port of the RTSP server to 1234
-				Editor editor = PreferenceManager.getDefaultSharedPreferences(
-						getApplicationContext()).edit();
-				editor.putString(RtspServer.KEY_PORT, String.valueOf(1234));
-				editor.commit();
+		// Sets the port of the RTSP server to 1234
+		Editor editor = PreferenceManager.getDefaultSharedPreferences(
+				getApplicationContext()).edit();
+		editor.putString(RtspServer.KEY_PORT, String.valueOf(1234));
+		editor.commit();
 
-				// Configures the SessionBuilder
-				SessionBuilder.getInstance().setSurfaceView(mSurfaceView)
-						.setPreviewOrientation(90)
-						.setContext(getApplicationContext())
-						.setAudioEncoder(SessionBuilder.AUDIO_NONE)
-						.setVideoEncoder(SessionBuilder.VIDEO_H264);
-				// Starts the RTSP server
-				getApplicationContext().startService(
-						new Intent(getApplicationContext(), RtspServer.class));
-			}
+		// Configures the SessionBuilder
+		SessionBuilder.getInstance().setSurfaceView(mSurfaceView)
+				.setPreviewOrientation(90).setContext(getApplicationContext())
+				.setAudioEncoder(SessionBuilder.AUDIO_NONE)
+				.setVideoEncoder(SessionBuilder.VIDEO_H264);
+		// Starts the RTSP server
+		getApplicationContext().startService(
+				new Intent(getApplicationContext(), RtspServer.class));
+	}
 
 	@Override
-	public void onPrepared(MediaPlayer mp) {
-		// TODO Auto-generated method stub
-		
+	protected void onPause() {
+		super.onPause();
+
+		// Stop RTSP server if it is running
+		getApplicationContext().stopService(new Intent(this, RtspServer.class));
 	}
 
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
 			int height) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onBitrareUpdate(long bitrate) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onSessionError(int reason, int streamType, Exception e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onPreviewStarted() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onSessionConfigured() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onSessionStarted() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onSessionStopped() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		
-	}
 }
