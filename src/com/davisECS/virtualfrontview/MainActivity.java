145535be521e8180f5b4a56b073432d48eb7f047
Matthew Yoshio Morikawa <mymorika@localhost.localdomain>
2014-06-01 16:26:09 -0700

    Added the modified libstreaming for data collection purposes

--- src/com/davisECS/virtualfrontview/MainActivity.java
+++ src/com/davisECS/virtualfrontview/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		
 		setContentView(R.layout.activity_main);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
